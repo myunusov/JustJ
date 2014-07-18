0b95ecd7c0125652c52cd82aef03a621961f0b84
myunusov <myunusov@luxoft.com>
2014-07-18 16:37:42 +0700

    JustJ-10::Create project

---
+++ justj-core/src/main/java/org/maxur/jj/core/domain/Context.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 Maxim Yunusov
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.maxur.jj.core.domain;
+
+/**
+ * @author Maxim Yunusov
+ * @version 1.0
+ * @since <pre>7/18/2014</pre>
+ */
+public class Context {
+
+    private static Context system = new Context();
+
+    public static Context system() {
+        return system;
+    }
+
+    public void runWith(final Config config, final String[] args) {
+        final Command start = getCommandMapper().getFor(args);
+        start.execute();
+    }
+
+    protected CommandMapper getCommandMapper() {   // TODO Must be injected
+        return new CommandMapper();
+    }
+}
