0c9e20f62a4cdd3541c5f5e1b1722664a7684fe0
Maxim Yunusov <maximyunusov@gmail.com>
2014-08-23 20:38:39 +0700

    justj-14 DEV: @Inject annotation support: Refactoring

--- justj-core/src/main/java/javax/inject/Provider.java
+++ justj-core/src/main/java/javax/inject/Provider.java
@@ -50,7 +50,7 @@ public interface Provider<T> {
      *
      * @throws RuntimeException if the injector encounters an error while
      *  providing an instance. For example, if an injectable member on
-     *  {@code T} throws an exception, the injector may reference the exception
+     *  {@code T} throws an exception, the injector may wrap the exception
      *  and throw it to the caller of {@code get()}. Callers should not try
      *  to handle such exceptions as the behavior may vary across injector
      *  implementations and even different configurations of the same injector.
